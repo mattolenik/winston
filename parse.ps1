452c8170dcc9fd1c1ab0958056c97f95c7bb94de
Matt Olenik <olenikm@gmail.com>
2015-07-26 23:41:23 -0700

    Appveyor script fix

---
+++ parse.ps1
@@ -0,0 +1,9 @@
+﻿[xml]$res = Get-Content .\results.xml
+foreach($c in $res.Contexts.Context) {
+    $spec = $c.Context.Specs.Spec
+    foreach($s in $spec) {
+        $name = "$($c.Name) $($s.Name)"
+        $status = $s.Status
+        Add-AppveyorTest -Name "$name" -Outcome "$status"
+    }
+}
