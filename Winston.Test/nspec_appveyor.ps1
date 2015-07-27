aaa93d1734078685d7a159aa0170513a36f8413f
Matt Olenik <olenikm@gmail.com>
2015-07-26 21:39:29 -0700

    AppVeyor test script

---
+++ Winston.Test/nspec_appveyor.ps1
@@ -0,0 +1,6 @@
+param (
+	[string]$configuration = "Release"
+)
+. "$(resolve-path ..\packages\nspec.*\tools\NSpecRunner.exe)" --formatter=XUnitFormatter bin\$configuration\Winston.Test.dll > results.xml
+$wc = New-Object 'System.Net.WebClient'
+$wc.UploadFile("https://ci.appveyor.com/api/testresults/xunit/$($env:APPVEYOR_JOB_ID)", (Resolve-Path .\results.xml))
