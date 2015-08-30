e20987f9da9b7791cec1fcf91779bdd36a1aacf4
Matt Olenik <olenikm@gmail.com>
2015-08-30 15:26:37 -0700

    Add .NET install state detection, output animation
    Former-commit-id: 06f10425336f7e600c6dfc079dd4fb5f4e241608

---
+++ winston_install/detectfx.h
@@ -0,0 +1,13 @@
+#pragma once
+extern bool IsCurrentOSTabletMedCenter();
+extern bool IsNetfx10Installed();
+extern bool IsNetfx11Installed();
+extern bool IsNetfx20Installed();
+extern bool IsNetfx30Installed();
+extern bool IsNetfx35Installed();
+extern bool IsNetfx40ClientInstalled();
+extern bool IsNetfx40FullInstalled();
+extern bool IsNetfx45Installed();
+extern bool IsNetfx451Installed();
+extern bool IsNetfx452Installed();
+extern bool IsNetfx46Installed();
