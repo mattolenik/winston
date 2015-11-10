017ad1765ced843a9a905d967ad8fe75b5066a02
Matt Olenik <olenikm@gmail.com>
2015-11-09 18:05:20 -0800

    CI fix attempt

--- tests_appveyor.ps1
+++ tests_appveyor.ps1
@@ -1,24 +1,18 @@
 param (
 	[string]$configuration = "Release"
 )
+
 $testAssembly="Winston.Test\bin\$configuration\Winston.Test.dll"
 
-function RunNSpec {
-    . "$(resolve-path .\packages\nspec.*\tools\NSpecRunner.exe)" --formatter=XmlFormatter $testAssembly > nspec_results.xml
-    [xml]$res = Get-Content .\nspec_results.xml
-    foreach($c in $res.Contexts.Context) {
-        $spec = $c.Context.Specs.Spec
-        foreach($s in $spec) {
-            $name = "$($c.Name) $($s.Name)"
-            $status = $s.Status
-            Add-AppveyorTest -Name $name -Outcome $status
-        }
+& "$(resolve-path .\packages\nspec.*\tools\NSpecRunner.exe)" --formatter=XmlFormatter "$testAssembly" > nspec_results.xml
+[xml]$res = Get-Content .\nspec_results.xml
+foreach($c in $res.Contexts.Context) {
+    $spec = $c.Context.Specs.Spec
+    foreach($s in $spec) {
+        $name = "$($c.Name) $($s.Name)"
+        $status = $s.Status
+        Add-AppveyorTest -Name $name -Outcome $status
     }
 }
 
-function RunMSTest {
-    vstest.console /logger:Appveyor $testAssembly
-}
-
-RunNSpec
-RunMSTest
+vstest.console /logger:Appveyor "$testAssembly"
