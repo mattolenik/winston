0d402dc6f3fa695b41e5ff78d47dfdc4a256f77e
Matt Olenik <olenikm@gmail.com>
2015-11-09 19:14:58 -0800

    Improve test reporting

--- tests_appveyor.ps1
+++ tests_appveyor.ps1
@@ -4,6 +4,7 @@ param (
 
 $testAssembly="Winston.Test\bin\$configuration\Winston.Test.dll"
 
+echo "Running NSpec tests"
 & "$(resolve-path .\packages\nspec.*\tools\NSpecRunner.exe)" --formatter=XmlFormatter "$testAssembly" > nspec_results.xml
 [xml]$res = Get-Content .\nspec_results.xml
 foreach($c in $res.Contexts.Context) {
@@ -11,7 +12,16 @@ foreach($c in $res.Contexts.Context) {
     foreach($s in $spec) {
         $name = "$($c.Name) $($s.Name)"
         $status = $s.Status
-        Add-AppveyorTest -Name $name -Outcome $status
+        $error = ""
+        $stackTrace = ""
+        if ($status -eq "Failed") {
+            $ex = $s.Exception.InnerText
+            # Remove newlines for stacktrace parameter
+            $stackTrace = [System.Text.RegularExpressions.Regex]::Replace($ex, "\r\n?|\n", "\\")
+            # Use first line of error for message
+            $message = $ex.Split([System.Environment]::NewLine)[0]
+        }
+        Add-AppveyorTest -Name "$name" -Outcome "$status" -FileName "$(Get-Item $testAssembly | select -ExpandProperty Name)" -ErrorMessage "$message" -ErrorStackTrace "$stackTrace"
     }
 }
 
