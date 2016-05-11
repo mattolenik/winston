f096a6d82ecd7af8b9490a4de2a0912a1474a122
Matt Olenik <olenikm@gmail.com>
2016-05-11 16:34:02 -0700

    Reimplement ParseQueryString to avoid extraneous dependencies
    For some reason these kept pulling in Newtonsoft.JSON.dll as well.

---
+++ Winston/Net/Extensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.Text.RegularExpressions;
+
+namespace Winston.Net
+{
+    static class Extensions
+    {
+        public static NameValueCollection ParseQueryString(this Uri uri)
+        {
+            var result = new NameValueCollection();
+            var query = uri.Query;
+
+            // remove anything other than query string from url
+            if (query.Contains("?"))
+            {
+                query = query.Substring(query.IndexOf('?') + 1);
+            }
+
+            foreach (string vp in Regex.Split(query, "&"))
+            {
+                var singlePair = Regex.Split(vp, "=");
+                if (singlePair.Length == 2)
+                {
+                    result.Add(singlePair[0], singlePair[1]);
+                }
+                else
+                {
+                    // only one key with no value specified in query string
+                    result.Add(singlePair[0], string.Empty);
+                }
+            }
+
+            return result;
+        }
+    }
+}
